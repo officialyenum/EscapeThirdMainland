0ee578c338470b1deda3aa0b5afc59b43b52d495
Chukwuyenum Opone <oponechukwuyenum@gmail.com>
2024-02-24 16:31:26 +0000

    Hiya

---
+++ EscapeThirdMainland/Source/EscapeThirdMainland/Private/Character/ShooterCharacterBase.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/ShooterCharacterBase.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/InputComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
+// Sets default values
+AShooterCharacterBase::AShooterCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->InitCapsuleSize(40.0f, 95.0f);
+	TurnRate = 45.0f;
+	LookUpRate = 45.0f;
+
+	FirstPersonCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("First Person Camera"));
+	FirstPersonCamera->SetupAttachment(GetCapsuleComponent());
+	FirstPersonCamera->AddRelativeLocation(FVector(-39.65f, 1.75f, 64.0f));
+
+	FirstPersonCamera->bUsePawnControlRotation = true;
+
+	HandsMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Character Mesh"));
+	HandsMesh->SetOnlyOwnerSee(true);
+	HandsMesh->SetupAttachment(FirstPersonCamera);
+	HandsMesh->bCastDynamicShadow = false;
+	HandsMesh->CastShadow = false;
+	HandsMesh->AddRelativeRotation(FRotator(1.9f, -19.19f, 5.2f));
+	HandsMesh->AddRelativeLocation(FVector(-0.5f, -4.4f, -155.7f));
+
+	GunMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Gun Mesh"));
+	GunMesh->SetOnlyOwnerSee(true);
+	GunMesh->bCastDynamicShadow = false;
+	GunMesh->CastShadow = false;
+
+	MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("Muzzle Location"));
+	MuzzleLocation->SetupAttachment(GunMesh);
+	MuzzleLocation->AddRelativeLocation(FVector(0.2f, 48.4f, -10.6f));
+
+	GunOffset = FVector(100.0f, 0.0f, 10.0f);
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	GunMesh->AttachToComponent(HandsMesh, FAttachmentTransformRules::SnapToTargetIncludingScale, TEXT("GripPoint"));
+	
+}
+
+// Called every frame
+void AShooterCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AShooterCharacterBase::OnFire()
+{
+}
+
+void AShooterCharacterBase::MoveForward(float Value)
+{
+}
+
+void AShooterCharacterBase::MoveRight(float Value)
+{
+}
+
+void AShooterCharacterBase::TurnAtRate(float Rate)
+{
+}
+
+void AShooterCharacterBase::LookAtRate(float Rate)
+{
+}
+
